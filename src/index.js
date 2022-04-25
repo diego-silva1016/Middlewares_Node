30579b0114627d4dd1cd6ebcc5548e0d8a0fba40
Diego Ribeiro <diego2222110@gmail.com>
2022-04-25 20:42:48 -0300

    Criando os middlewares

--- src/index.js
+++ src/index.js
@@ -10,19 +10,61 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+    const { username } = request.headers;
+
+    const userExist = users.find(user => user.username === username);
+    
+    if(!userExist)
+      return response.status(404).json({error: 'Usuário não existe.'})
+
+    request.user = userExist;
+
+    return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  if(!user.pro && user.todos.length === 10)
+    return response.status(403).json({error: 'Você ja atingiu o limite de todos cadastrados, ative o plano pro para continuar.'})
+
+  return next();
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  const userExist = users.find(user => user.username === username);
+    
+  if(!userExist)
+    return response.status(404).json({error: 'Usuário não existe.'});
+
+  if(!validate(id))
+    return response.status(400).json({error: 'O id informado não é válido.'});
+
+  const todo = userExist.todos.find(td => td.id === id);
+
+  if(!todo)
+    return response.status(404).json({error: 'O todo não existe.'})
+
+  request.user = userExist;
+  request.todo = todo;
+
+  return next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const userExist = users.find(user => user.id === id);
+    
+  if(!userExist)
+    return response.status(404).json({error: 'Usuário não existe.'});
+
+  request.user = userExist;
+
+  return next();
 }
 
 app.post('/users', (request, response) => {
